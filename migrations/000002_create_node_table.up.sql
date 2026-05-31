fc2ef8ef10c731105216d28ad281997390f2094b
Sujay Chanageri <suju.narayan@gmail.com>
2026-05-31 07:06:59 +0000

    added repoitory functions for nodes repository

--- migrations/000002_create_node_table.up.sql
+++ migrations/000002_create_node_table.up.sql
@@ -1,8 +1,11 @@
+CREATE TYPE ntype AS ENUM ('shard', 'replica');
+
 CREATE TABLE nodes (
     id UUID PRIMARY KEY,
     project_id UUID NOT NULL,
-    node_name VARCHAR NOT NULL,
+    node_name VARCHAR(255) NOT NULL,
     node_index INTEGER NOT NULL,
     node_status BOOLEAN NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-)
+    node_type ntype NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
